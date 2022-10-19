271d2c7205ebdcd197dd3e3ebf6c101a277cec46
Saima_Siddique <saimasiddique@stamford.university>
2022-10-19 13:03:28 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,42 @@
+const prompt = require("prompt-sync")();
+
+// Numeric
+var num = 123;
+// String 
+var ch = "I am a web developer";
+// Boolean 
+var isPresent = true;
+// console.log(num);
+// console.log(ch);
+// console.log(isPresent);
+// var userInput = prompt();
+// console.log(userInput);
+
+var x,y,res;
+
+x = 10;
+y = 20;
+
+//  var a = parseInt(prompt('Enter your first number '));
+//  var b = parseInt(prompt('Enter your second number '));
+
+// res = x+y;
+// console.log(res);
+// console.log(typeof (x+y));
+// console.log(typeof (a+b));
+
+// console.log(x+y);
+// console.log(a+b);
+
+let temp = 30;
+const age = 18;
+// console.log (temp);
+// console.log(age);
+
+// temp= 25;
+// age = 20;
+
+
+
+
+
