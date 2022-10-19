78dd004dab36fbebc60c441ccaca7485f4927c59
Saima_Siddique <saimasiddique@stamford.university>
2022-10-19 13:12:41 +0600

    condition added

---
+++ more-js.js
@@ -0,0 +1,19 @@
+const prompt = require("prompt-sync")();
+
+// let age = parseInt(prompt("enter your age"));
+// // console.log(typeof age);
+//  if (age == 18)
+//  {
+//     console.log('sweet 18 age');
+//  }
+//  else if (age == 20){
+//     console.log('you are growing up');
+//  }
+//  else if (age > 20){
+//     console.log('You are an adult');
+//  }
+//  else{
+//     console.log( 'You are kid');
+//  }
+
+
